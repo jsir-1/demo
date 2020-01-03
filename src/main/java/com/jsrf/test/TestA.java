f59941392fcc485897ee8395e643493e1d9dfa0a
jsrf <jishirifang@meituan.com>
2020-01-03 17:48:32 +0800

    C

--- src/main/java/com/jsrf/test/TestA.java
+++ src/main/java/com/jsrf/test/TestA.java
@@ -10,4 +10,20 @@ package com.jsrf.test;
 public class TestA {
     String name;
     int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }

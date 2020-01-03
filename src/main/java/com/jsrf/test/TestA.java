6b04e68df288405d6dea964f76b2009622f90863
jsrf <jishirifang@meituan.com>
2020-01-03 17:45:49 +0800

    A

---
+++ src/main/java/com/jsrf/test/TestA.java
@@ -0,0 +1,11 @@
+package com.jsrf.test;
+
+/**
+ * Desc:请输入类描述信息
+ * ------------------------------------
+ *
+ * @author:jishirifang@meituan.com Date:2020/1/3
+ * Time:5:45 下午
+ */
+public class TestA {
+}
